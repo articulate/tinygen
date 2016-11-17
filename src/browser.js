486325b2f876fbcabd1383364809959ec9d6aac6
Scott McCormack <scott.mccormack@articulate.com>
2016-11-17 13:09:44 -0500

    First stab at browser implementation

---
+++ src/browser.js
@@ -0,0 +1,14 @@
+const chars  = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz',
+      crypto = global.crypto || global.msCrypto
+
+const tinygen = (len=16) => {
+  const arr = new Uint32Array(len)
+  crypto.getRandomValues(arr)
+  return Array.from(arr).map(x => chars[x % 64]).join('')
+}
+
+const unsupported = () => {
+  throw new Error('Secure random number generation not supported by this browser. Use Chrome, FireFox, or Internet Explorer 11.')
+}
+
+module.exports = crypto && crypto.getRandomValues ? tinygen : unsupported
